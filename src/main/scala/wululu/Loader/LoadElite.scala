278c8db46fdf05d355d96e2c07786006c7e8eda8
Rousseau Emile <emilerousseau16@gmail.com>
2026-01-27 13:06:24 +0100

    loading of yelp.elite into df

---
+++ src/main/scala/wululu/Loader/LoadElite.scala
@@ -0,0 +1,26 @@
+package wululu
+
+import com.typesafe.config.ConfigFactory
+import org.apache.spark.sql.{DataFrame}
+
+import wululu.SparkSessionWrapper.spark
+
+object LoadElite {
+    def main(args: Array[String]): Unit = {
+        val config = ConfigFactory.load("pg_source.conf")
+        val dbConfig = config.getConfig("pg_source")
+
+        val df: DataFrame = spark.read
+            .format("jdbc")
+            .option("url", dbConfig.getString("url"))
+            .option("user", dbConfig.getString("user"))
+            .option("password", dbConfig.getString("password"))
+            .option("dbtable", dbConfig.getString("elite_table.table_name"))
+            .option("driver", dbConfig.getString("driver"))
+            .load()
+
+        println(df.show(10))
+        println(df.count())
+        println("We got here !")
+    }
+}
